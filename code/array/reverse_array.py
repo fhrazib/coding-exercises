7c2fd9c4cff83e945b8ba044e8c02458faa65ab7
Razib <razib.fh@gmail.com>
2021-07-30 19:28:48 +0600

    reverse array in different ways

--- code/array/reverse_array.py
+++ code/array/reverse_array.py
@@ -1,3 +1,11 @@
+"""
+reverse_v1:
+- reversing array using simple naive method
+- time complexity O(n)
+- space complexity O(n)
+"""
+
+
 def reverse_v1(arr):
     result = []
     start = len(arr) - 1
@@ -6,18 +14,40 @@ def reverse_v1(arr):
     while start >= end:
         result.append(arr[start])
         start = start - 1
-
     return result
+
+
+"""
+reverse_v2:
+- reversing array using better technique
+- time complexity O(n)
+- space complexity O(1)
+"""
+
+
+def reverse_v2(arr):
+    start = 0
+    end = len(arr) - 1
+
+    while end > start:
+        arr[start], arr[end] = arr[end], arr[start]
+        start = start + 1
+        end = end - 1
+    return arr
 
 
 def main():
     array = [11, 34, 98, 25, 29, 38, 99]
+    # array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     print('The original array: ', array)
-    #
-    # array.reverse()
-    # print('After reversing the array: ', array)
 
-    print(reverse_v1(array))
+    # reversed_array = reverse_v1(array)
+    reversed_array = reverse_v2(array)
+    print('After reversing the array: ', array)
+
+    # array[::-1]
+    # finally array.reverse() also reverse the array in space, it doesnt' return anything
 
 
 if __name__ == "__main__":
