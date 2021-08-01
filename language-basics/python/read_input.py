71a6db82d28261aa70ccab95904d689cd0bc5e85
Razib <razib.fh@gmail.com>
2021-08-01 14:31:15 +0600

    reading input

--- language-basics/python/read_input.py
+++ language-basics/python/read_input.py
@@ -1,3 +1,2 @@
-https://www.hackerrank.com/challenges/equal-stacks/submissions/code/197908809
-
-https://www.hackerrank.com/challenges/ctci-ransom-note/submissions/code/199099770
+# https://www.hackerrank.com/challenges/equal-stacks/submissions/code/197908809
+# https://www.hackerrank.com/challenges/ctci-ransom-note/submissions/code/199099770
