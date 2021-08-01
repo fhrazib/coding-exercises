94ef1d999154bedcca5bc52e2e7d91f9e6143334
Razib <razib.fh@gmail.com>
2021-08-01 22:33:38 +0600

    adding python code

--- language-basics/python/map_and_filter.py
+++ language-basics/python/map_and_filter.py
@@ -9,6 +9,11 @@ NOTE:
     - map doesnt return an ITERABLE
     - map process EACH element of an ITERABLE.
     - though you pass an whole ITERABLE to a map, map itself send EACH ELEMENT from that iterable to your given function
+
+
+filter(function, iterable)
+    - filter filters out only those elements from the iterator for which the given function returns true
+    - (while map apply the given function to each element of the iterable)
 """
 
 
@@ -46,6 +51,13 @@ def main():
     map_obj3 = map(make_pairs, index, fruits)
     print(list(map_obj3))  # ['1:apple', '2:orange', '3:banana', '4:lemon', '5:mango', '6:pineapple', '7:cherry']
     print(dict(map_obj3))  # {}
+
+    # map with double argument lambda
+    print('double argument lambda: ', list(map(lambda a, b: a + b, [1, 2, 3, 7, 9], [1, 1, 4, 6, 6])))
+
+    # filter out only the even numbers
+    nums = [23, 39, 36, 37, 38, 47, 48, 22, 96, 1002, 10001, 2041, 20, 2089, 1]
+    print('the even numbers from the list are: ', list(filter(lambda x: x % 2 == 0, nums)))
     pass
 
 
