1dd8c0bd8b4a5a028174cceb4fa266dea8fca984
Razib <razib.fh@gmail.com>
2021-08-06 14:35:02 +0600

    leetcode 25, 167

---
+++ problems/leetcode/leetcode_125.py
@@ -0,0 +1,26 @@
+"""
+Name: Remove element
+URL: https://leetcode.com/problems/valid-palindrome/
+Tags: #array
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        char_array = [c.lower() for c in s if c.isalnum()]
+
+        start = 0
+        end = len(char_array) - 1
+        while start <= end:
+            if char_array[start] != char_array[end]:
+                return False
+            start = start + 1
+            end = end - 1
+
+        return True
+
+#
+# s = Solution()
+# s1 = "A man, a plan, a canal: Panama"  # True
+# s2 = "race a car"  # False
+# print(s.isPalindrome(s2))
---
+++ problems/leetcode/leetcode_167.py
@@ -0,0 +1,32 @@
+"""
+Name: Two Sum II - Input array is sorted
+URL: https://leetcode.com/problems/two-sum-ii-input-array-is-sort
+Tags: #array, #two-pointers
+"""
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        result = []
+
+        start = 0
+        end = len(numbers) - 1
+        while start <= end:
+            _sum = numbers[start] + numbers[end]
+            if target == _sum:
+                result = [start + 1, end + 1]
+                break
+            elif _sum > target:
+                end = end - 1
+            elif _sum < target:
+                start = start + 1
+
+        return result
+
+#
+# s = Solution()
+# a, at = [2, 7, 11, 15], 9  # [1, 2]
+# b, bt = [2, 3, 4], 6  # [1, 3]
+# c, ct = [-1, 0], -1  # [1, 2]
+# print(s.twoSum(a, at))
