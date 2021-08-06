93818b016e776772080fc83a96353cf3dc2f012a
Razib <razib.fh@gmail.com>
2021-08-06 12:01:33 +0600

    leetcode 118

---
+++ problems/leetcode/leetcode_118.py
@@ -0,0 +1,31 @@
+"""
+Name: Pascal's Triangle
+URL: https://leetcode.com/problems/pascals-triangle/
+Tags: #array, #dictionary, #map, #DP
+"""
+from typing import List
+
+_map = {1: [1], 2: [1, 1]}
+
+
+class Solution:
+    def generate(self, num_rows: int) -> List[List[int]]:
+        for i in range(3, 31):
+            previous_row = _map.get(i - 1)
+            new_row = [1]
+            for j in range(1, len(previous_row)):
+                new_row.append(previous_row[j - 1] + previous_row[j])
+
+            new_row.append(1)
+            _map[i] = new_row
+
+        result = []
+        for i in range(1, num_rows + 1):
+            row = _map.get(i)
+            result.append(row)
+
+        return result
+
+#
+# s = Solution()
+# print(s.generate(4))
